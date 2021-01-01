f40e8b3ae81d03e05d892029bdfcc18bd5701168
ethanschreur <65560924+ethanschreur@users.noreply.github.com>
2021-01-01 14:09:17 -0500

    DOM manipulation practice

---
+++ dom_manipulation/DOM-practice.js
@@ -0,0 +1,29 @@
+// Select the section with an id of container without using querySelector.
+let containerId = document.getElementById('container');
+// Select the section with an id of container using querySelector.
+let containerQuery = document.querySelector('#container');
+// Select all of the list items with a class of “second”.
+let secondClassList = document.getElementsByClassName('second');
+// Select a list item with a class of third, but only the list item inside of the ol tag.
+let listItemClassThird = document.querySelector('ol .third');
+// Give the section with an id of container the text “Hello!”.
+containerId.innerText = 'Hello!';
+// Add the class main to the div with a class of footer.
+let divClassFooter = document.querySelector('.footer');
+divClassFooter.classList.add('main');
+// Remove the class main on the div with a class of footer.
+divClassFooter.classList.remove('main');
+// Create a new li element.
+let newLi = document.createElement('li');
+// Give the li the text “four”.
+newLi.innerText = 'four';
+// Append the li to the ul element.
+let ulElement = document.querySelector('ul');
+ulElement.append(newLi);
+// Loop over all of the lis inside the ol tag and give them a background color of “green”.
+let liListInOL = document.querySelectorAll('ol li');
+for (let li of liListInOL) {
+	li.getElementsByClassName.backgroundColor = 'green';
+}
+// Remove the div with a class of footer
+divClassFooter.remove();
